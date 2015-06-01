db8e69e060b73e412772db924c2a2e2f1fa9c854
Do Ba <ba_do@knorex.com>
2015-06-01 11:42:36 +0700

    First Push

---
+++ src/com/ikaratruyen/slidingmenu/anim/CustomScaleAnimation.java
@@ -0,0 +1,19 @@
+package com.ikaratruyen.slidingmenu.anim;
+
+import android.graphics.Canvas;
+
+import com.ikaratruyen.R;
+import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu.CanvasTransformer;
+
+public class CustomScaleAnimation extends CustomAnimation {
+
+	public CustomScaleAnimation() {
+		super(R.string.anim_scale, new CanvasTransformer() {
+			@Override
+			public void transformCanvas(Canvas canvas, float percentOpen) {
+				canvas.scale(percentOpen, 1, 0, 0);
+			}			
+		});
+	}
+
+}
---
+++ src/com/ikaratruyen/utils/Utils.java
@@ -0,0 +1,134 @@
+package com.ikaratruyen.utils;
+
+import java.io.StringWriter;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import android.app.Activity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.util.DisplayMetrics;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ikaratruyen.R;
+import com.ikaratruyen.model.Book;
+import com.ikaratruyen.utils.IkaraConstant.DEVICETYPE;
+//DUNG THU VIEN JACKSON http://wiki.fasterxml.com/JacksonHome
+
+
+public class Utils {
+	private static final Logger log = Logger.getLogger(Utils.class
+			.getName());
+	static ObjectMapper mapper = new ObjectMapper();
+	
+//	public static ArrayList<SearchItem> getSearchData(JSONArray arr){
+//		ArrayList<SearchItem> datas = new ArrayList<SearchItem>();
+//		for(int i = 0; i < arr.length(); i++){
+//			SearchItem item = new SearchItem();
+//			item.title = arr.optJSONObject(i).names();
+//		}
+//		return datas;
+//	}
+	
+	public static int detectDeviceType(Context context){
+		
+		String value = context.getResources().getString(R.string.screen_type);
+		int deviceType = -1;
+		if (value.equals("phone")){
+			deviceType = DEVICETYPE.PHONE;
+		}else if (value.equals("7-inch-tablet")){
+			deviceType = DEVICETYPE.TABLET7INCH;
+		}else{
+			if (value.equals("10-inch-tablet")){
+				deviceType = DEVICETYPE.TABLET10INCH;
+			}
+		}
+		
+		return deviceType;
+
+	}
+
+	public static boolean hasNetworkConnection(Context context) {
+		if (context == null)
+			return false;
+		boolean haveConnectedWifi = false;
+		boolean haveConnectedMobile = false;
+
+		ConnectivityManager cm = (ConnectivityManager) context
+				.getSystemService(Context.CONNECTIVITY_SERVICE);
+		if (cm != null) {
+			NetworkInfo[] netInfo = cm.getAllNetworkInfo();
+			if (netInfo != null) {
+				for (NetworkInfo ni : netInfo) {
+					if (ni.getTypeName().equalsIgnoreCase("WIFI"))
+						if (ni.isConnected())
+							haveConnectedWifi = true;
+					if (ni.getTypeName().equalsIgnoreCase("MOBILE"))
+						if (ni.isConnected())
+							haveConnectedMobile = true;
+				}
+			}
+		}
+
+		return haveConnectedWifi || haveConnectedMobile;
+	}
+
+	
+	public static String serialize(Object object){
+		if (object == null) return null;
+		
+		StringWriter writer = new StringWriter();
+		try {
+			mapper.writeValue(writer, object);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return writer.getBuffer().toString();	
+	}
+	
+	public static <T> T deserialize(Class<T> type, String string){
+		if (string == null) return null;
+		try {
+			return (T)mapper.readValue(string,type);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			log.log(Level.WARNING, "BUGS", e);
+			e.printStackTrace();
+		}
+		return null;
+	}
+	
+	public void sortBookFollowTime(ArrayList<Book> books){
+		Collections.sort(books, new Comparator<Book>() {
+
+			@Override
+			public int compare(Book c1, Book c2) {
+				// TODO Auto-generated method stub
+				DateFormat f = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+				try {
+					String day1 = c1.savedTime;//c1.getIssue_release_date().split("T")[0];
+					String day2 = c2.savedTime;//c2.getIssue_release_date().split("T")[0];
+
+					return f.parse(day2).compareTo(f.parse(day1));
+				} catch (ParseException e) {
+					throw new IllegalArgumentException(e);
+				}
+			}
+		});
+	}
+	
+	public static int dpToPx(Activity context, int dp) {
+	    DisplayMetrics displayMetrics = context.getResources().getDisplayMetrics();
+	    int px = Math.round(dp * (displayMetrics.xdpi / DisplayMetrics.DENSITY_DEFAULT));       
+	    return px;
+	} 
+	
+}
